841cf2a81797e3debf348760611859946e6534ec
anurtaza <156662880+anurtaza@users.noreply.github.com>
2025-04-10 13:17:48 +0500

    Update login.component.ts

--- edu-connect/src/app/login/login.component.ts
+++ edu-connect/src/app/login/login.component.ts
@@ -1,21 +1,32 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  username: string = '';
+  email: string = '';
   password: string = '';
+  errorMessage: string = '';
 
   constructor(private authService: AuthService, private router: Router) {}
 
   login() {
-    this.authService.login(this.username, this.password).subscribe(() => {
-      this.router.navigate(['/']);
+    if (!this.email || !this.password) {
+      this.errorMessage = 'Please fill all fields';
+      return;
+    }
+
+    this.authService.login(this.email, this.password).subscribe({
+      next: () => {
+        this.router.navigate(['/courses']);
+      },
+      error: () => {
+        this.errorMessage = 'Invalid login or password';
+      }
     });
   }
 }
